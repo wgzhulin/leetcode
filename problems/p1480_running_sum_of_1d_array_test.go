7fc12b2743a507594fb033a83bf41f1753ab96a7
zhulinw <51152640+zhulinw@users.noreply.github.com>
2022-07-15 23:30:54 +0800

    leetcode problem 1480

---
+++ problems/p1480_running_sum_of_1d_array_test.go
@@ -0,0 +1,32 @@
+package problems
+
+import (
+	"github.com/zhulinw/leetcode/testify/assert"
+	"testing"
+)
+
+func TestRunningSum(t *testing.T) {
+	type data struct {
+		input  []int
+		except []int
+	}
+
+	testData := []data{
+		{
+			input:  []int{1, 2, 3, 4},
+			except: []int{1, 3, 6, 10},
+		},
+		{
+			input:  []int{1, 1, 1, 1, 1},
+			except: []int{1, 2, 3, 4, 5},
+		},
+		{
+			input:  []int{1},
+			except: []int{1},
+		},
+	}
+
+	for _, tdata := range testData {
+		assert.Equal(t, tdata.except, runningSum(tdata.input))
+	}
+}
