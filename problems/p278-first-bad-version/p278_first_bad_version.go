d9191c24c2f9ccc9cf6fa48b72f82876030cbba3
zhulinw <51152640+zhulinw@users.noreply.github.com>
2022-07-21 22:49:03 +0800

    leetcode problem 278

---
+++ problems/p278-first-bad-version/p278_first_bad_version.go
@@ -0,0 +1,30 @@
+package first_bad_version
+
+/*
+278. 第一个错误的版本
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/first-bad-version/
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*/
+func firstBadVersion(n int) int {
+	left, right := 1, n
+	for left < right {
+		mid := left + (right-left)/2
+		if isBadVersion(mid) {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return left
+}
+
+var stub int
+
+func isBadVersion(n int) bool {
+	if n == stub {
+		return true
+	}
+	return false
+}
---
+++ problems/p278-first-bad-version/p278_first_bad_version_test.go
@@ -0,0 +1,38 @@
+package first_bad_version
+
+import (
+	"github.com/zhulinw/leetcode/testify/assert"
+	"testing"
+)
+
+func TestIsBadVersion(t *testing.T) {
+	type Input struct {
+		Para1 int
+		Para2 int
+	}
+
+	type Output struct {
+		Para1 int
+	}
+
+	input := []Input{
+		{
+			Para1: 5,
+			Para2: 4,
+		},
+		{
+			Para1: 1,
+			Para2: 1,
+		},
+	}
+
+	output := []Output{
+		{Para1: 4},
+		{Para1: 1},
+	}
+
+	for i := range input {
+		stub = input[i].Para2
+		assert.Equal(t, output[i].Para1, firstBadVersion(input[i].Para1))
+	}
+}
