2d6fbc75380089be0692d62a112bcb81a5925380
zhulinw <51152640+zhulinw@users.noreply.github.com>
2022-07-23 22:11:38 +0800

    fix problem 278

--- problems/p278-first-bad-version/p278_first_bad_version.go
+++ problems/p278-first-bad-version/p278_first_bad_version.go
@@ -9,7 +9,7 @@ package first_bad_version
 */
 func firstBadVersion(n int) int {
 	left, right := 1, n
-	for left < right {
+	for left <= right {
 		mid := left + (right-left)/2
 		if isBadVersion(mid) {
 			right = mid - 1
